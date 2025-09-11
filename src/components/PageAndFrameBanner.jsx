dc7d7c1eb091e4cdfcce47b6bd898c5fac49bacb
Candela <candelamarzaroli@gmail.com>
2025-09-11 14:29:55 -0300

    login y register

---
+++ src/components/PageAndFrameBanner.jsx
@@ -0,0 +1,31 @@
+
+import "./Banner.css"; // Asegúrate de importar los estilos
+
+const PageAndFrameBanner = () => (
+  <div className="texturizer-content">
+    <div
+      id="ransomizer-n3zcjhub"
+      style={{
+        textAlign: "left",
+        fontSize: "45px",
+      }}
+    >
+      <div className="ts">
+        <div className="tw">
+          <div className="mlvx-0 tc"><div>P</div></div>
+          <div className="mlvx-1 tc"><div>a</div></div>
+          <div className="mlvx-2 tc"><div>g</div></div>
+          <div className="mlvx-3 tc"><div>e</div></div>
+          <div className="mlvx-4 tc"><div>&</div></div>
+          <div className="mlvx-5 tc"><div>F</div></div>
+          <div className="mlvx-6 tc"><div>r</div></div>
+          <div className="mlvx-7 tc"><div>a</div></div>
+          <div className="mlvx-8 tc"><div>m</div></div>
+          <div className="mlvx-9 tc"><div>e</div></div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default PageAndFrameBanner;
