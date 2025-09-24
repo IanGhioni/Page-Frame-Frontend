488f9e7e0a4a1426ff96d4f0fe939e5823d2d784
Chiara Forti Dono <cfortidono@uvq.edu.ar>
2025-09-24 19:56:46 -0300

    se cancela

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,13 +1,10 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import API from "../service/api";
 import "./user.css";
 import Navbar from "../components/navBar/NavBar";
 import { getFotoPerfil } from "../FotoPerfilMapper";
 
 const User = () => {
    const navigate = useNavigate();
-   const [listas, setListas] = useState([]);
 
    const logout = () => {
       localStorage.clear();
@@ -19,22 +16,6 @@ const User = () => {
    };
 
    const fotoPerfil = getFotoPerfil(localStorage.getItem("fotoPerfil"));
-
-   const fetchListas = () => {
-      const idUser = localStorage.getItem("id");
-      API.verLista(idUser)
-         .then((response) => {
-            console.log(`Listas del usuario:`, response.data);
-            setListas(response.data);
-         })
-         .catch((error) => {
-            console.error(`Error al obtener la lista`, error);
-         });
-   };
-
-   useEffect(() => {
-      fetchListas();
-   }, []);
 
    return (
       <div>
@@ -55,11 +36,6 @@ const User = () => {
          <button onClick={() => irALista("QUIERO VER")}>QUIERO VER</button>
          <button onClick={() => irALista("LEIDO")}>LEIDO</button>
          <button onClick={() => irALista("QUIERO LEER")}>QUIERO LEER</button>
-         {listas && listas.length > 0 && listas.map((lista, idx) => (
-            <button key={idx} onClick={() => irALista(lista.nombre)}>
-               {lista.nombre}
-            </button>
-         ))}
 
       </div>
    );
